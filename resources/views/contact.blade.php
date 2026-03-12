3f940987a9fdd974995e58c6ee6fa3f393c6d215
BAGUS_SATRIO <bagussatrio1102@gmail.com>
2026-03-12 17:20:43 +0700

    Praktikum MVC Laravel

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<h1>Hubungi kami</h1>
+<p>silakan hubungi kami di:</p>
+<p>Email: Bagussatrio1102@gmail.com</P>
+<p>Phone: 08123456789</p>
+
+<a href="/">kembali ke home</a>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<h1>selamat datang di {{ $nama }}</h1>
+<p> website penyewaan kamar dan hotel terbaik.</p>
+<a href="/contact">Contact</a>
